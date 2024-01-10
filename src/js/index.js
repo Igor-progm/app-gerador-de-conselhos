56df2d3b4ed83a6fd3ee995a622ed67e9e01b570
Igor Antunes <igor.antunes39@hotmail.com>
2024-01-10 15:23:18 -0300

    subindo projeto completo

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const dadoBotao = document.getElementById('btn')
+const idConselho = document.getElementById('id')
+const descricaoConselho = document.getElementById('advice-description')
+
+async function buscarConselho() {
+    const url = "https://api.adviceslip.com/advice"
+    const resposta = await fetch(url)
+    return await resposta.json()
+}
+
+async function tirarUmConselho() {
+    const conselhos = await buscarConselho()
+    idConselho.innerHTML = `Advice #${conselhos.slip.id}`
+    descricaoConselho.innerHTML = conselhos.slip.advice
+    gerenciarDisabled()
+}
+
+dadoBotao.addEventListener('click', tirarUmConselho)
+
+function gerenciarDisabled() {
+    dadoBotao.setAttribute('disabled', "")
+
+    setTimeout(() => {
+        dadoBotao.removeAttribute('disabled')
+    }, 2000);
+}
